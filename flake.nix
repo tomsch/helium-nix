8b3c5fc191834f1ed389043e4dd916656f8d9942
ori <meow@ori.cat>
2026-07-29 15:44:15 +0200

    add aarch64 support

--- flake.nix
+++ flake.nix
@@ -3,18 +3,31 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.${system} = {
-        default = pkgs.callPackage ./package.nix {};
-        helium = self.packages.${system}.default;
-      };
-
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages."x86_64-linux" =
+        let
+          system = "x86_64-linux";
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.callPackage ./package-x86_64.nix { };
+          helium = self.packages.${system}.default;
+        };
+      packages."aarch64-linux" =
+        let
+          system = "aarch64-linux";
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.callPackage ./package-aarch64.nix { };
+          helium = self.packages.${system}.default;
+        };
       overlays.default = final: prev: {
-        helium = final.callPackage ./package.nix {};
+        helium = final.callPackage (
+          if final.stdenv.hostPlatform.isAarch64 then ./package-aarch64.nix else ./package-x86_64.nix
+        ) { };
       };
     };
 }
